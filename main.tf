d20fbc923973a9f681e37fd88c8f17ae26a8ec35
Isaac Diaz <isaac_diazhdz@outlook.com>
2022-09-17 02:30:15 -0400

    Add Cluster

--- main.tf
+++ main.tf
@@ -21,3 +21,35 @@ resource "azurerm_resource_group" "gtest" {
   name     = "${var.RGName}"
   location =  "${var.location}"
 }
+
+resource "azurerm_kubernetes_cluster" "gtest_k8s_cluster" {
+  name                = "gtest_k8s_cluster"
+  location            = azurerm_resource_group.gtest.location
+  resource_group_name = azurerm_resource_group.gtest.name
+  dns_prefix          = "gtest_aks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.gtest_k8s_cluster.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.gtest_k8s_cluster.kube_config_raw
+
+  sensitive = true
+}
